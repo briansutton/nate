a4b2e47db1c9d0fac59de44e32e9bc7de1d88878
Brian <bsutton@rmc.edu>
2020-01-09 09:22:09 -0500

    Scale by capacity when computing barycentric weights.

--- baryweights.m
+++ baryweights.m
@@ -13,14 +13,14 @@ function ws = baryweights(xs)
 %     WS: a vector of barycentric weights for use in the barycentric
 %     interpolation formula
 %
-%   Copyright 2019 Brian Sutton
+%   Copyright 2020 Brian Sutton
 
 narginchk(1,1);
 natecheck('baryweights',xs);
 m = length(xs)-1;
+C = (max(xs)-min(xs))/4;
 ws = nan(m+1,1);
 for i = 0:m
   k = [0:i-1 i+1:m];
-  ws(i+1) = 1/prod(xs(i+1)-xs(k+1));
+  ws(i+1) = 1/prod((xs(i+1)-xs(k+1))/C);
 end
-
